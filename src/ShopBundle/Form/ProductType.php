0e51d928ff9a7a3a1c76f1079db0cc2c4dfebf93
tai_ng <pianotai@gmail.com>
2015-01-28 16:22:05 +0100

    commit after CRUD Product generation

---
+++ src/ShopBundle/Form/ProductType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ShopBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProductType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('productName')
+            ->add('productPrice')
+            ->add('productPic')
+            ->add('productDescription')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ShopBundle\Entity\Product'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'shopbundle_product';
+    }
+}
